d26b7ef02e2612353de9cb442571d667bf225b15
ShivamP-97 <shivampathak19972@gmail.com>
2025-11-29 21:23:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const num1 = prompt("Enter the first number:");
+const num2 = prompt("Enter the second number:");
+
+const result = Number(num1) === Number(num2);
+
+alert(result);
